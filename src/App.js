f1e40cc38be6ed6efd23c58958d7d462f3594176
Anna <aiurchyk@gmail.com>
2023-10-03 17:22:58 -0400

    lesson 1.1 HW

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+
+function getTitle(title) {
+  return title;
+}
+
+const toDoList = [
+  {
+    id: '1',
+    title: 'Complete HW 1',
+  },
+  {
+    id: '2',
+    title: 'Pick up Sofia from GYM',
+  },
+  {
+    id: '3',
+    title: 'Read React book',
+  },
+  {
+    id: '4',
+    title: 'Relax',
+  },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello {getTitle('Todo list')}</h1>
+
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type="text" />
+
+      <hr />
+
+      <ul>
+        {toDoList.map(function(item) {
+          return (
+            <li key={item.id}>
+              {item.id} {item.title}
+            </li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
